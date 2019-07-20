45ba1f6d9affb3f7ae976ea407d84fd7fe742e73
Samgu <orz311013@gmail.com>
2019-07-20 15:15:11 +0800

    0720 Practice Commit

--- app/src/main/java/com/samgu/relativelayout/MainActivity.java
+++ app/src/main/java/com/samgu/relativelayout/MainActivity.java
@@ -2,16 +2,19 @@ package com.samgu.relativelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtComPlay, txtResult;
-    Button btnScissors, btnStone, btnPaper, btnMax, btnSave, btnDisplay;
-    int count = 0, win = 0, lose = 0, draw = 0; //初始化 局數 贏 輸 平手
+    Button btnScissors, btnStone, btnPaper, btnMax, btnSave, btnDisplay, btnClear;
+    int count = 0, win = 0, lose = 0, draw = 0; //初始化 局數 贏 輸 平手,
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +32,73 @@ public class MainActivity extends AppCompatActivity {
         btnMax = findViewById(R.id.btnMax);
         btnSave = findViewById(R.id.btnSave);
         btnDisplay = findViewById(R.id.btnDisplay);
+        btnClear = findViewById(R.id.btnClear);
 
         btnScissors.setOnClickListener(ScissorsLin);
         btnStone.setOnClickListener(StoneLin);
         btnPaper.setOnClickListener(PaperLin);
+        btnSave.setOnClickListener(SaveLin);
+        btnDisplay.setOnClickListener(DisplayLin);
+        btnClear.setOnClickListener(ClearLin);
+        btnMax.setOnClickListener(MaxLin);
     }
+
+    private Button.OnClickListener MaxLin = new Button.OnClickListener() {
+        public void onClick(View view) {
+            SharedPreferences gameResult = getSharedPreferences("game", 0);
+            int max = gameResult.getInt("Win", 0); //(存檔裡的)得到Win的值 初始值為0
+            if (win >= max) //如果贏數 >= 最高分
+                max = win; //那最高分就= 贏數
+            //Toast.makeText(getApplicationContext(), max, Toast.LENGTH_SHORT).show();
+            txtResult.setText("最高分為:" + max + "分");
+        }
+    };
+
+    private Button.OnClickListener ClearLin = new Button.OnClickListener() {
+        public void onClick(View view) {
+            SharedPreferences gameResult = getSharedPreferences("gmae", 0);
+            gameResult.edit().remove("Win").commit(); //清除
+//gameReslut.edit().remove("Win").commit(); 可以移除""某個key
+
+        }
+    };
+
+
+    private Button.OnClickListener DisplayLin = new Button.OnClickListener() {
+        public void onClick(View view) {
+            StringBuilder sb = new StringBuilder();
+
+            float b = (float) win / count * 100;
+            //勝率 浮點數
+            sb.append("局數:" + count + "\n");
+            sb.append("贏:" + win + "\n");
+            sb.append("輸:" + lose + "\n");
+            sb.append("平手:" + draw + "\n");
+            sb.append("勝率為:" + String.valueOf(b) + "%");  // float b 浮點數轉字串 > Stirng.valueOf()
+
+
+            Toast.makeText(getApplicationContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+            txtResult.setText(sb.toString());
+        }
+    };
+
+
+    private Button.OnClickListener SaveLin = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            SharedPreferences gameResult = getSharedPreferences("game", 0);
+            //String "檔名" ,mode 0 只能供讀 1可給別人讀取但是需要知道位置 2可讀寫給別人但需要知道位置
+            //SharedPreferences 存取偏好檔案的內容，該檔案儲存在內部儲存體中
+            gameResult.edit()//編輯  <-return this回傳物件本身 故能可以直接接下去寫方法
+                    .putInt("Count", count)
+                    .putInt("Win", win)
+                    .putInt("Lose", lose)
+                    .putInt("Draw", draw)
+                    .apply();//提交
+            Toast.makeText(getApplicationContext(), "存檔成功", Toast.LENGTH_SHORT).show();
+        }
+    };
+
 
     private Button.OnClickListener ScissorsLin = new Button.OnClickListener() {
         @Override
@@ -76,7 +141,6 @@ public class MainActivity extends AppCompatActivity {
                 lose++;
                 txtComPlay.setText("布");
                 txtResult.setText("結果:輸");
-
             }
         }
     };
